b951b189f5953392c061f9850b4faf731a84a513
Mathias Lux <dermotte@users.noreply.github.com>
2012-12-12 11:10:18 +0000

    Added JointHistogram feature and fixed bug in JSD implementation.

--- src/test/java/net/semanticmetadata/lire/imageanalysis/TestJointHistogram.java
+++ src/test/java/net/semanticmetadata/lire/imageanalysis/TestJointHistogram.java
@@ -4,9 +4,9 @@ import junit.framework.TestCase;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * User: mlux
@@ -15,36 +15,26 @@ import java.io.IOException;
  */
 public class TestJointHistogram extends TestCase {
     public void testExtract() throws IOException {
-        int[][] histogram = new int[64][9];
-        for (int i = 0; i < histogram.length; i++) {
-            for (int j = 0; j < histogram[i].length; j++)
-                histogram[i][j] = 0;
-        }
         BufferedImage img = ImageIO.read(new FileInputStream("C:\\Java\\Projects\\LireSVN\\testdata\\ferrari\\black\\2828686873_2fa36f83d7_b.jpg"));
-        WritableRaster raster = img.getRaster();
-        int[] px = new int[3];
-        for (int x = 1; x < raster.getWidth() - 1; x++) {
-            for (int y = 1; y < raster.getHeight() - 1; y++) {
-                raster.getPixel(x, y, px);
-                int colorPos = (int) Math.round((double) px[2] / 85d) +
-                        (int) Math.round((double) px[1] / 85d) * 4 +
-                        (int) Math.round((double) px[0] / 85d) * 4 * 4;
-                int rank = 0;
-                double intensity = getIntensity(px);
-                if (getIntensity(raster.getPixel(x - 1, y - 1, px)) > intensity) rank++;
-                if (getIntensity(raster.getPixel(x, y - 1, px)) > intensity) rank++;
-                if (getIntensity(raster.getPixel(x + 1, y - 1, px)) > intensity) rank++;
-                if (getIntensity(raster.getPixel(x - 1, y + 1, px)) > intensity) rank++;
-                if (getIntensity(raster.getPixel(x, y + 1, px)) > intensity) rank++;
-                if (getIntensity(raster.getPixel(x + 1, y + 1, px)) > intensity) rank++;
-                if (getIntensity(raster.getPixel(x - 1, y, px)) > intensity) rank++;
-                if (getIntensity(raster.getPixel(x + 1, y, px)) > intensity) rank++;
-                histogram[colorPos][rank]++;
-            }
-        }
+        JointHistogram jh = new JointHistogram();
+        jh.extract(img);
+        System.out.println(Arrays.toString(jh.descriptor));
+
+        JointHistogram jh2 = new JointHistogram();
+        jh2.setByteArrayRepresentation(jh.getByteArrayRepresentation());
+
+        System.out.println(jh2.getDistance(jh));
+
     }
 
-    private double getIntensity(int[] px) {
-        return (0.3 * px[0] + 0.6 * px[1] + 0.1 * px[2]);
+    public void testJsd() {
+        int i = 0;
+        float[] h1 = new float[]{1f};
+        float[] h2 = new float[]{6f};
+        float result = (float) ((h1[i] > 0 ? (h1[i]) * Math.log((2f * h1[i]) / (h1[i] + h2[i])) : 0) +
+                (h2[i] > 0 ? (h2[i]) * Math.log((2f * h2[i]) / (h1[i] + h2[i])) : 0));
+        System.out.println("result = " + result);
+        System.out.println(h1[i] > 0 ? "1" : "0");
     }
+
 }
